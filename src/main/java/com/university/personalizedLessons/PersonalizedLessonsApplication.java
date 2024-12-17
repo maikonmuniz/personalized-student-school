2e481ab56d883cd58b07db54e4e0b7f11dd03e2a
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-17 00:53:41 -0300

    refactor: modify .gitignore

---
+++ src/main/java/com/university/personalizedLessons/PersonalizedLessonsApplication.java
@@ -0,0 +1,13 @@
+package com.university.personalizedLessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizedLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizedLessonsApplication.class, args);
+	}
+
+}
