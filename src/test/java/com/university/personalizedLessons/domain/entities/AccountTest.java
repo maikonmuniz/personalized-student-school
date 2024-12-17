ec9ebbc77b7ce3d6c73c379e158c394fc171394c
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-17 20:28:44 -0300

    test: create account

--- src/test/java/com/university/personalizedLessons/domain/entities/AccountTest.java
+++ src/test/java/com/university/personalizedLessons/domain/entities/AccountTest.java
@@ -1,4 +1,20 @@
+package com.university.personalizedLessons.domain.entities;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AccountTest {
-  
+
+    @Test
+    public void generateAccount() {
+        Account account = new Account("maikon", "muniz", "34242342344234");
+        assertEquals("maikon", account.getFirstName());
+        assertEquals("muniz", account.getLastName());
+        assertEquals("34242342344234", account.getCpf());
+    }
 }
