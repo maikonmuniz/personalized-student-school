2da34779315208b920e0e0027d55ac3a33855f8c
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 15:49:19 -0300

    feat: create model account

--- src/main/java/com/university/personalizedLessons/infrastructure/models/AccountModel.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/models/AccountModel.java
@@ -1,26 +1,23 @@
-package com.university.personalizedLessons.infrastructure.repository.model;
+package com.university.personalizedLessons.infrastructure.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Setter
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Table(name = "account")
+@Entity
 public class AccountModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
+    @Column(name = "username", length = 100)
     private String username;
 
+    @Column(name = "password", length = 100)
     private String password;
 
     @Column(name = "first_name", length = 100)
@@ -31,4 +28,44 @@ public class AccountModel {
 
     @Column(name = "cpf", length = 11)
     private String cpf;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
