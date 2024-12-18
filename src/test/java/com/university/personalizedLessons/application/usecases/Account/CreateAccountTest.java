db0c6dc3664363b9f2923f8936737ad7285b1f92
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 16:17:54 -0300

    test: implementation test to test create account

--- src/test/java/com/university/personalizedLessons/application/usecases/Account/CreateAccountTest.java
+++ src/test/java/com/university/personalizedLessons/application/usecases/Account/CreateAccountTest.java
@@ -1,11 +1,20 @@
 package com.university.personalizedLessons.application.usecases.Account;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.university.personalizedLessons.domain.entities.Account;
+import com.university.personalizedLessons.infrastructure.models.AccountModel;
+import com.university.personalizedLessons.infrastructure.operationORM.AccountJPA;
+import com.university.personalizedLessons.infrastructure.repository.AccountRepo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class CreateAccountTest {
@@ -13,24 +22,42 @@ class CreateAccountTest {
     @InjectMocks
     private CreateAccount createAccount;
 
-    String firstName = "Maikon";
-    String lastName = "Muniz";
-    String cpf = "435242234234";
+    @Mock
+    private AccountRepo accountRepo;
 
     @Test
+    @DisplayName("Should save entity in data base!")
     public void createAccount () {
 
-        CreateAccount.Input input = new CreateAccount.Input(
-                "Maikon",
-                "Muniz",
-                "435242234234"
+        AccountJPA accountJPAMock = mock(AccountJPA.class);
+
+        AccountRepo accountRepo = new AccountRepo(accountJPAMock);
+
+        Account account = new Account(
+                "maikon",
+                "muniz",
+                "123456789",
+                "maikon@maikon",
+                "senha123"
         );
 
-        CreateAccount.Output output = createAccount.execute(input);
+        AccountModel savedAccountModel = new AccountModel();
+        savedAccountModel.setFirstName("maikon");
+        savedAccountModel.setLastName("muniz");
+        savedAccountModel.setCpf("123456789");
+        savedAccountModel.setUsername("maikon@maikon");
+        savedAccountModel.setPassword("senha123");
 
-        assertEquals(firstName, output.firstName());
-        assertEquals(lastName, output.lastName());
-        assertEquals(cpf, output.cpf());
+        when(accountJPAMock.save(any(AccountModel.class))).thenReturn(savedAccountModel);
+
+        Account savedAccount = accountRepo.save(account);
+
+        assertNotNull(savedAccount);
+        assertEquals("maikon", savedAccount.getFirstName());
+        assertEquals("muniz", savedAccount.getLastName());
+        assertEquals("123456789", savedAccount.getCpf());
+        assertEquals("maikon@maikon", savedAccount.getUsername());
+        assertEquals("senha123", savedAccount.getPassword());
 
     }
 }
