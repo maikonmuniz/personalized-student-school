3dcf3d5870b5f7ceed777665957f7412ff87d6e0
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-17 22:46:02 -0300

    feat: create repo to save entity Account

---
+++ src/main/java/com/university/personalizedLessons/infrastructure/repository/AccountRepo.java
@@ -0,0 +1,12 @@
+package com.university.personalizedLessons.infrastructure.repository;
+
+import com.university.personalizedLessons.application.repository.AccountRepository;
+import com.university.personalizedLessons.domain.entities.Account;
+
+public class AccountRepo implements AccountRepository {
+
+    @Override
+    public Account save(Account account) {
+        return null;
+    }
+}
