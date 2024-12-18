a719c4f1b2ea4b7bd69afb4fcd6da03d384fafc8
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 16:16:43 -0300

    feat: create implements repository account

--- src/main/java/com/university/personalizedLessons/infrastructure/repository/AccountRepo.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/repository/AccountRepo.java
@@ -2,11 +2,35 @@ package com.university.personalizedLessons.infrastructure.repository;
 
 import com.university.personalizedLessons.application.repository.AccountRepository;
 import com.university.personalizedLessons.domain.entities.Account;
+import com.university.personalizedLessons.infrastructure.models.AccountModel;
+import com.university.personalizedLessons.infrastructure.operationORM.AccountJPA;
 
 public class AccountRepo implements AccountRepository {
 
+    private final AccountJPA accountJPA;
+
+    public AccountRepo(AccountJPA accountJPA) {
+        this.accountJPA = accountJPA;
+    }
+
     @Override
     public Account save(Account account) {
-        return null;
+
+        AccountModel accountModel = new AccountModel();
+        accountModel.setUsername(account.getFirstName());
+        accountModel.setLastName(account.getLastName());
+        accountModel.setCpf(account.getCpf());
+        accountModel.setUsername(account.getUsername());
+        accountModel.setPassword(account.getPassword());
+
+        AccountModel accountModelRegister = accountJPA.save(accountModel);
+
+        return new Account(
+                accountModelRegister.getFirstName(),
+                accountModelRegister.getLastName(),
+                accountModelRegister.getCpf(),
+                accountModelRegister.getUsername(),
+                accountModelRegister.getPassword()
+        );
     }
 }
