4641fedd63e8cc3a0810127449813932cbc4b99c
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 15:49:00 -0300

    refactor: add fields user and password

--- src/main/java/com/university/personalizedLessons/domain/entities/Account.java
+++ src/main/java/com/university/personalizedLessons/domain/entities/Account.java
@@ -5,15 +5,21 @@ public class Account {
     private final String firstName;
     private final String lastName;
     private final String cpf;
+    private final String username;
+    private final String password;
 
     public Account (
             String firstName,
             String lastName,
-            String cpf
+            String cpf,
+            String username,
+            String password
     ) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.cpf = cpf;
+        this.username = username;
+        this.password = password;
     }
 
     public String getFirstName() {
@@ -26,5 +32,13 @@ public class Account {
 
     public String getCpf() {
         return cpf;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
