ba770161329e654e4e0aab315b369eaf83f217ee
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-17 22:35:07 -0300

    feat: create bean to instance class the usecase CreateAccount

--- src/main/java/com/university/personalizedLessons/main/MainCreateAccount.java
+++ src/main/java/com/university/personalizedLessons/main/MainCreateAccount.java
@@ -1,4 +1,14 @@
 package com.university.personalizedLessons.main;
 
+import com.university.personalizedLessons.application.usecases.Account.CreateAccount;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainCreateAccount {
+
+    @Bean
+    CreateAccount createAccount () {
+        return new CreateAccount();
+    }
 }
