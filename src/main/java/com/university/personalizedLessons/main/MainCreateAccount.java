4b9c097244e0b89bd287f30082378a6061411431
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 16:17:09 -0300

    feat: inversion depedency

--- src/main/java/com/university/personalizedLessons/main/MainCreateAccount.java
+++ src/main/java/com/university/personalizedLessons/main/MainCreateAccount.java
@@ -1,6 +1,8 @@
 package com.university.personalizedLessons.main;
 
 import com.university.personalizedLessons.application.usecases.Account.CreateAccount;
+import com.university.personalizedLessons.infrastructure.operationORM.AccountJPA;
+import com.university.personalizedLessons.infrastructure.repository.AccountRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +10,12 @@ import org.springframework.context.annotation.Configuration;
 public class MainCreateAccount {
 
     @Bean
-    CreateAccount createAccount () {
-        return new CreateAccount();
+    CreateAccount createAccount (AccountRepo accountRepo) {
+        return new CreateAccount(accountRepo);
+    }
+
+    @Bean
+    AccountRepo createAccountRepo (AccountJPA accountJPA) {
+        return new AccountRepo(accountJPA);
     }
 }
