ea423ac26e054179bec34ff2be2b71bab66328d5
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-17 22:44:14 -0300

    feat: create repository contracts

---
+++ src/main/java/com/university/personalizedLessons/application/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.university.personalizedLessons.application.repository;
+
+import com.university.personalizedLessons.domain.entities.Account;
+
+public interface AccountRepository {
+    Account save (Account account);
+}
