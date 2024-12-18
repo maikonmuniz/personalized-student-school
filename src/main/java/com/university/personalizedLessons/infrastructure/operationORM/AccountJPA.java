f098e57cdd87a4ac9f9a3b6f5a636abf3b818c82
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 15:50:54 -0300

    feat: create account operation jpa

--- src/main/java/com/university/personalizedLessons/infrastructure/operationORM/AccountJPA.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/operationORM/AccountJPA.java
@@ -1,4 +1,7 @@
 package com.university.personalizedLessons.infrastructure.operationORM;
 
-public class AccountJPA {
+import com.university.personalizedLessons.infrastructure.models.AccountModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountJPA extends JpaRepository <AccountModel, Long> {
 }
