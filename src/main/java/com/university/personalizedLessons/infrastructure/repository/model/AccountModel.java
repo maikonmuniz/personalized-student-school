257d5a47206966da0569708dc7540eeb6d7c79a0
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 00:43:40 -0300

    feat: create model account

---
+++ src/main/java/com/university/personalizedLessons/infrastructure/repository/model/AccountModel.java
@@ -0,0 +1,34 @@
+package com.university.personalizedLessons.infrastructure.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Column(name = "first_name", length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", length = 100)
+    private String lastName;
+
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+}
