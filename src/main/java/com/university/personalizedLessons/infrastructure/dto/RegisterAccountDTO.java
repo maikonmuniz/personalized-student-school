f70aabc93bad7b9e1bd03c80ad6922f07ee6ea41
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 22:26:23 -0300

    feat: create dto to register

---
+++ src/main/java/com/university/personalizedLessons/infrastructure/dto/RegisterAccountDTO.java
@@ -0,0 +1,9 @@
+package com.university.personalizedLessons.infrastructure.dto;
+
+public record RegisterAccountDTO (
+        String firstName,
+        String lastName,
+        String cpf,
+        String username,
+        String password
+) { }
---
+++ src/main/java/com/university/personalizedLessons/infrastructure/http/AccountController.java
@@ -0,0 +1,4 @@
+package com.university.personalizedLessons.infrastructure.http;
+
+public class AccountController {
+}
