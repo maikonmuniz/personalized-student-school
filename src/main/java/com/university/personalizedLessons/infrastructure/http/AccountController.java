569de276a335f73bdcddcf6fef8a49d5e8261fe3
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-18 22:26:46 -0300

    feat: create controller to register account

--- src/main/java/com/university/personalizedLessons/infrastructure/http/AccountController.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/http/AccountController.java
@@ -1,4 +1,31 @@
 package com.university.personalizedLessons.infrastructure.http;
 
+import com.university.personalizedLessons.application.usecases.Account.CreateAccount;
+import com.university.personalizedLessons.infrastructure.dto.RegisterAccountDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("account")
 public class AccountController {
+
+    @Autowired
+    CreateAccount createAccount;
+
+    @PostMapping("register")
+    public ResponseEntity registerAccount (@RequestBody RegisterAccountDTO data) {
+        CreateAccount.Output output = this.createAccount.execute(
+                new CreateAccount.Input(
+                        data.firstName(),
+                        data.lastName(),
+                        data.cpf(),
+                        data.username(),
+                        data.password()
+                )
+        );
+
+        return ResponseEntity.ok(output);
+
+    }
 }
