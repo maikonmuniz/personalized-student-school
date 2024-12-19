6327bb11b8fd4363ebdea94faea25a62199b6264
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-19 19:47:22 -0300

    feat: create jpa model course

--- src/main/java/com/university/personalizedLessons/infrastructure/models/Course.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/models/Course.java
@@ -1,4 +1,33 @@
 package com.university.personalizedLessons.infrastructure.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "course")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(length = 250)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "type_course_id")
+    private TypeCourse typeCourse;
+
 }
