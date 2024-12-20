a05f3d0d4842d3f0f4357beb70a8b7525267276e
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:45:13 -0300

    feat: create usecase create register account in course

--- src/main/java/com/university/personalizedLessons/application/usecases/Account/RegisterAccountInCourse.java
+++ src/main/java/com/university/personalizedLessons/application/usecases/Account/RegisterAccountInCourse.java
@@ -1,7 +1,39 @@
 package com.university.personalizedLessons.application.usecases.Account;
 
-public class RegisterCourse {
+import com.university.personalizedLessons.domain.entities.account.Account;
+import com.university.personalizedLessons.domain.entities.course.CourseAggregate;
+import com.university.personalizedLessons.infrastructure.repository.AccountRepo;
+import com.university.personalizedLessons.infrastructure.repository.CourseRepo;
 
+public class RegisterAccountInCourse {
 
+    AccountRepo accountRepo;
+    CourseRepo courseRepo;
 
+    public RegisterAccountInCourse (
+            AccountRepo accountRepo,
+            CourseRepo courseRepo
+    ) {
+        this.accountRepo = accountRepo;
+        this.courseRepo = courseRepo;
+    }
+
+    public Output execute (Input input) {
+        Account account = this.accountRepo.findAccount(input.username);
+        CourseAggregate course = this.courseRepo.findCourse(input.idCourse);
+        return new Output(
+                account.getUsername(),
+                course.getId()
+        );
+    }
+
+    public static record Input (
+            String username,
+            int idCourse
+    ) {}
+
+    public static record Output (
+            String username,
+            int idCourse
+    ) {}
 }
