2306c54239c62c4a17f46b0f99057e93ee741332
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 07:00:10 -0300

    feat: inversion depedency register account in course

--- src/main/java/com/university/personalizedLessons/main/account/MainRegisterAccountInCourse.java
+++ src/main/java/com/university/personalizedLessons/main/account/MainRegisterAccountInCourse.java
@@ -1,4 +1,22 @@
 package com.university.personalizedLessons.main.account;
 
+import com.university.personalizedLessons.application.usecases.Account.RegisterAccountInCourse;
+import com.university.personalizedLessons.infrastructure.repository.AccountRepo;
+import com.university.personalizedLessons.infrastructure.repository.CourseRepo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainRegisterAccountInCourse {
+
+    @Bean
+    RegisterAccountInCourse instanceRegisterAccountInCourse(
+            AccountRepo accountRepo,
+            CourseRepo courseRepo
+    ) {
+        return new RegisterAccountInCourse(
+                accountRepo,
+                courseRepo
+        );
+    }
 }
