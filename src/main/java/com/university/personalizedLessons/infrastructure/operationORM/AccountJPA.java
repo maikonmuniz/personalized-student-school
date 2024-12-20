f809ca35fd41d3ac506ca775744b3b460aaaf4db
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:54:20 -0300

    refactor: accountJpa modify findUsername to findByUsername

--- src/main/java/com/university/personalizedLessons/infrastructure/operationORM/AccountJPA.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/operationORM/AccountJPA.java
@@ -4,4 +4,5 @@ import com.university.personalizedLessons.infrastructure.models.AccountModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountJPA extends JpaRepository <AccountModel, Long> {
+    AccountModel findByUsername(String username);
 }
