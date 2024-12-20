8a4d93cc06565e5d2556fb6b6217da80184ce76f
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:47:39 -0300

    feat: create models AccountCourse

--- src/main/java/com/university/personalizedLessons/infrastructure/models/AccountCourse.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/models/AccountCourse.java
@@ -1,4 +1,44 @@
 package com.university.personalizedLessons.infrastructure.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "account_course")
 public class AccountCourse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_course", nullable = false)
+    private CourseModel course;
+
+    @ManyToOne
+    @JoinColumn(name = "id_account", nullable = false)
+    private AccountModel account;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CourseModel getCourse() {
+        return course;
+    }
+
+    public void setCourse(CourseModel course) {
+        this.course = course;
+    }
+
+    public AccountModel getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountModel account) {
+        this.account = account;
+    }
 }
