dd5037008067d00af8dd6dea76850956bdce1dce
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 17:46:26 -0300

    feat: add table accountCourse

--- src/main/resources/struct.sql
+++ src/main/resources/struct.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE `account` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
@@ -7,19 +6,30 @@ CREATE TABLE `account` (
   `last_name` varchar(100) NOT NULL,
   `cpf` varchar(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `account_course` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id_course` int NOT NULL,
+  `id_account` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_course` (`id_course`),
+  KEY `id_account` (`id_account`),
+  CONSTRAINT `account_course_ibfk_1` FOREIGN KEY (`id_course`) REFERENCES `course` (`id`),
+  CONSTRAINT `account_course_ibfk_2` FOREIGN KEY (`id_account`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `account_discipline` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `notes` decimal(2,2) NOT NULL,
   `id_discipline` int NOT NULL,
-  `id_account` bigint NOT NULL,
   `id_type_note` int NOT NULL,
+  `id_account_course` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_type_note_re` (`id_type_note`),
   KEY `id_discipline_refe` (`id_discipline`),
-  KEY `id_account_refe` (`id_account`),
-  CONSTRAINT `id_account_refe` FOREIGN KEY (`id_account`) REFERENCES `account` (`id`),
+  KEY `id_account_course_refe` (`id_account_course`),
+  CONSTRAINT `id_account_course_refe` FOREIGN KEY (`id_account_course`) REFERENCES `account_course` (`id`),
   CONSTRAINT `id_discipline_refe` FOREIGN KEY (`id_discipline`) REFERENCES `discipline` (`id`),
   CONSTRAINT `id_type_note_re` FOREIGN KEY (`id_type_note`) REFERENCES `type_note` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -50,11 +60,11 @@ CREATE TABLE `type_course` (
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `type_note` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(150) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
