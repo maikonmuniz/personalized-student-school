b9c4b51e45e7599c29c6778a17e885386952c6e9
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 07:00:46 -0300

    feat: repository course persisten aggregate

--- src/main/java/com/university/personalizedLessons/main/account/MainRepositoryCourse.java
+++ src/main/java/com/university/personalizedLessons/main/account/MainRepositoryCourse.java
@@ -1,4 +1,15 @@
 package com.university.personalizedLessons.main.account;
 
+import com.university.personalizedLessons.infrastructure.operationORM.CourseJpa;
+import com.university.personalizedLessons.infrastructure.repository.CourseRepo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainRepositoryCourse {
+
+    @Bean
+    public CourseRepo instanceCourseRepository(CourseJpa courseJpa) {
+        return new CourseRepo(courseJpa);
+    }
 }
