c67b240c9ab0266fd7b5bc7a0d1b6473e7e98ecd
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:58:23 -0300

    feat: EnrollmentRepo create repository persisten aggregate

--- src/main/java/com/university/personalizedLessons/infrastructure/repository/EnrollmentRepo.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/repository/EnrollmentRepo.java
@@ -1,6 +1,58 @@
-package com.university.personalizedLessons.infrastructure.operationORM;
+package com.university.personalizedLessons.infrastructure.repository;
 
 import com.university.personalizedLessons.application.repository.EnrollmentRepository;
+import com.university.personalizedLessons.domain.entities.RegisterCourse.Enrollment;
+import com.university.personalizedLessons.domain.entities.account.Account;
+import com.university.personalizedLessons.domain.entities.course.CourseAggregate;
+import com.university.personalizedLessons.infrastructure.models.AccountCourse;
+import com.university.personalizedLessons.infrastructure.models.AccountModel;
+import com.university.personalizedLessons.infrastructure.models.CourseModel;
+import com.university.personalizedLessons.infrastructure.models.TypeCourseModel;
+import com.university.personalizedLessons.infrastructure.operationORM.EnrollmentJpa;
 
-public interface EnrollmentRepo implements EnrollmentRepository {
+public class EnrollmentRepo implements EnrollmentRepository {
+
+    private final EnrollmentJpa enrollmentJpa;
+
+    public EnrollmentRepo (EnrollmentJpa enrollmentJpa) {
+        this.enrollmentJpa = enrollmentJpa;
+    }
+
+    @Override
+    public Enrollment save(Account account, CourseAggregate courseAggregate) {
+
+        AccountModel accountModel = new AccountModel();
+        accountModel.setFirstName(account.getFirstName());
+        accountModel.setLastName(account.getLastName());
+        accountModel.setCpf(account.getCpf());
+        accountModel.setUsername(account.getUsername());
+        accountModel.setPassword(account.getPassword());
+
+
+        CourseModel courseModel = new CourseModel();
+        courseModel.setName(courseAggregate.getName());
+        courseModel.setDescription(courseAggregate.getDescription());
+
+        TypeCourseModel typeCourseModel = new TypeCourseModel();
+        typeCourseModel.setId(courseAggregate.getTypeCourseId());
+
+        courseModel.setTypeCourse(typeCourseModel);
+
+        AccountCourse accountCourse = new AccountCourse();
+        accountCourse.setAccount(accountModel);
+        accountCourse.setCourse(courseModel);
+
+        accountCourse = this.enrollmentJpa.save(accountCourse);
+
+        Enrollment enrollment = new Enrollment(
+                accountCourse.getId(),
+                accountCourse.getCourse().getId(),
+                accountCourse.getAccount().getId()
+        );
+
+        this.enrollmentJpa.save(accountCourse);
+
+        return enrollment;
+    }
+
 }
