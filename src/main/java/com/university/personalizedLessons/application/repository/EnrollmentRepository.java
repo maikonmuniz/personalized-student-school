0d80741df394af4f6e1c4b4685cb4896688a752e
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:43:45 -0300

    feat: create contracts EnrollmentRepository

--- src/main/java/com/university/personalizedLessons/application/repository/EnrollmentRepository.java
+++ src/main/java/com/university/personalizedLessons/application/repository/EnrollmentRepository.java
@@ -1,7 +1,9 @@
 package com.university.personalizedLessons.application.repository;
 
 import com.university.personalizedLessons.domain.entities.RegisterCourse.Enrollment;
+import com.university.personalizedLessons.domain.entities.account.Account;
+import com.university.personalizedLessons.domain.entities.course.CourseAggregate;
 
 public interface EnrollmentRepository {
-    Enrollment save ()
+    Enrollment save (Account account, CourseAggregate courseAggregate);
 }
