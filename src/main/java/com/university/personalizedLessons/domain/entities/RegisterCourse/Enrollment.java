d454c39b8bf1987c9dd14094a757f8334ec2ca16
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:46:21 -0300

    feat: create entity Enrollment

--- src/main/java/com/university/personalizedLessons/domain/entities/RegisterCourse/Enrollment.java
+++ src/main/java/com/university/personalizedLessons/domain/entities/RegisterCourse/Enrollment.java
@@ -1,4 +1,30 @@
 package com.university.personalizedLessons.domain.entities.RegisterCourse;
 
 public class Enrollment {
+
+    private final long idAccount;
+    private final int idCourse;
+    private final Long id;
+
+    public Enrollment (
+            Long id,
+            int idCourse,
+            long idAccount
+    ) {
+        this.id = id;
+        this.idCourse = idCourse;
+        this.idAccount = idAccount;
+    }
+
+    public long getIdAccount() {
+        return idAccount;
+    }
+
+    public int getIdCourse() {
+        return idCourse;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
