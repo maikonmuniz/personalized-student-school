3dab90be4b82a2bb58d19ef8db0c2cefff3c34cf
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:51:43 -0300

    refactor: refactor TypeCourseModel

--- src/main/java/com/university/personalizedLessons/infrastructure/models/TypeCourseModel.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/models/TypeCourseModel.java
@@ -1,14 +1,9 @@
 package com.university.personalizedLessons.infrastructure.models;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -25,5 +20,28 @@ public class TypeCourseModel {
     @Column(length = 255)
     private String description;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
 
