f02a7217e25cd58091ee2662bc954bf5ef522747
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-20 06:57:43 -0300

    feat: create repository to persisten aggregate

--- src/main/java/com/university/personalizedLessons/infrastructure/repository/CourseRepo.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/repository/CourseRepo.java
@@ -1,4 +1,31 @@
 package com.university.personalizedLessons.infrastructure.repository;
 
-public class CourseRepo {
+import com.university.personalizedLessons.application.repository.CourseRepository;
+import com.university.personalizedLessons.domain.entities.course.CourseAggregate;
+import com.university.personalizedLessons.infrastructure.models.CourseModel;
+import com.university.personalizedLessons.infrastructure.operationORM.CourseJpa;
+
+import java.util.Optional;
+
+public class CourseRepo implements CourseRepository {
+
+    private final CourseJpa courseJpa;
+
+    public CourseRepo(
+            CourseJpa courseJpa
+    ) {
+        this.courseJpa = courseJpa;
+    }
+
+    @Override
+    public CourseAggregate findCourse(int id) {
+        Optional<CourseModel> courseModel = this.courseJpa.findById(id);
+
+        return courseModel.map(model -> new CourseAggregate(
+                model.getId(),
+                model.getName(),
+                model.getDescription(),
+                model.getTypeCourse().getId()
+        )).orElse(null);
+    }
 }
