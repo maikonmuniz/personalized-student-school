7bf4e74cb3a6dfde4a6e3679944668c3725e8134
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-25 20:36:59 -0300

    feat: add id in account repository operation

--- src/main/java/com/university/personalizedLessons/application/repository/AccountRepository.java
+++ src/main/java/com/university/personalizedLessons/application/repository/AccountRepository.java
@@ -1,7 +1,8 @@
 package com.university.personalizedLessons.application.repository;
 
-import com.university.personalizedLessons.domain.entities.Account;
+import com.university.personalizedLessons.domain.entities.account.Account;
 
 public interface AccountRepository {
     Account save (Account account);
+    Account findAccount(String username);
 }
