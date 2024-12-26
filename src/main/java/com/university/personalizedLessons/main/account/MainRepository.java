d36239439500e2e4bf45adaeaf3b7c01600b0076
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-25 23:10:40 -0300

    feat: create instance EnrollmentJpa

--- src/main/java/com/university/personalizedLessons/main/account/MainRepository.java
+++ src/main/java/com/university/personalizedLessons/main/account/MainRepository.java
@@ -1,4 +1,15 @@
 package com.university.personalizedLessons.main.account;
 
+import com.university.personalizedLessons.infrastructure.operationORM.EnrollmentJpa;
+import com.university.personalizedLessons.infrastructure.repository.EnrollmentRepo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainRepository {
+
+    @Bean
+    EnrollmentRepo InstanceEnrollmentRepo (EnrollmentJpa enrollmentJpa) {
+        return new EnrollmentRepo(enrollmentJpa);
+    }
 }
