4bd418761aae673bfd6f447617a45aace1e6dcfb
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-12-25 21:31:17 -0300

    feat: add composite the repository operation in inversion depedency

--- src/main/java/com/university/personalizedLessons/main/account/MainRegisterAccountInCourse.java
+++ src/main/java/com/university/personalizedLessons/main/account/MainRegisterAccountInCourse.java
@@ -3,6 +3,7 @@ package com.university.personalizedLessons.main.account;
 import com.university.personalizedLessons.application.usecases.Account.RegisterAccountInCourse;
 import com.university.personalizedLessons.infrastructure.repository.AccountRepo;
 import com.university.personalizedLessons.infrastructure.repository.CourseRepo;
+import com.university.personalizedLessons.infrastructure.repository.EnrollmentRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +13,13 @@ public class MainRegisterAccountInCourse {
     @Bean
     RegisterAccountInCourse instanceRegisterAccountInCourse(
             AccountRepo accountRepo,
-            CourseRepo courseRepo
+            CourseRepo courseRepo,
+            EnrollmentRepo enrollmentRepo
     ) {
         return new RegisterAccountInCourse(
                 accountRepo,
-                courseRepo
+                courseRepo,
+                enrollmentRepo
         );
     }
 }
