2019cc788949c4ca57bba90176288284fe5e361f
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-15 02:23:38 -0300

    feat: add in usecase createAccount

--- src/main/java/com/university/personalizedLessons/application/usecases/Account/CreateAccount.java
+++ src/main/java/com/university/personalizedLessons/application/usecases/Account/CreateAccount.java
@@ -2,25 +2,33 @@ package com.university.personalizedLessons.application.usecases.Account;
 
 import com.university.personalizedLessons.domain.entities.account.Account;
 import com.university.personalizedLessons.infrastructure.repository.AccountRepo;
+import com.university.personalizedLessons.infrastructure.springSecurityBcripty.CryptAdapter;
 
 public class CreateAccount {
 
     private final AccountRepo accountRepo;
+    private final CryptAdapter crypt;
 
     public CreateAccount (
-            AccountRepo accountRepo
+            AccountRepo accountRepo,
+            CryptAdapter crypt
     ) {
         this.accountRepo = accountRepo;
+        this.crypt = crypt;
     }
 
     public Output execute (Input input) {
+
+        String password = this.crypt.encrypt(
+                input.password
+        );
 
         Account account = new Account(
                 input.firstName,
                 input.lastName,
                 input.cpf,
                 input.username,
-                input.password
+                password
         );
 
         Account accountSave = this.accountRepo.save(account);
