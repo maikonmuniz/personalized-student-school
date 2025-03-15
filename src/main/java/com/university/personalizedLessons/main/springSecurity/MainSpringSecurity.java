d6c427c165e79a82a11e699995071333844f1523
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-15 02:22:37 -0300

    feat: add bean in class Adapter crypt and add in usecase createAccount

--- src/main/java/com/university/personalizedLessons/main/springSecurity/MainSpringSecurity.java
+++ src/main/java/com/university/personalizedLessons/main/springSecurity/MainSpringSecurity.java
@@ -1,4 +1,14 @@
 package com.university.personalizedLessons.main.springSecurity;
 
+import com.university.personalizedLessons.infrastructure.springSecurityBcripty.CryptAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainSpringSecurity {
+
+    @Bean
+    CryptAdapter createBcrypt () {
+        return new CryptAdapter();
+    }
 }
