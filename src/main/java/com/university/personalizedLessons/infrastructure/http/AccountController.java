9eca68a13e1484fb3b8d67ffd4b47c12dc93167e
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-17 02:30:56 -0300

    feat: add rote the login account

--- src/main/java/com/university/personalizedLessons/infrastructure/http/AccountController.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/http/AccountController.java
@@ -1,6 +1,8 @@
 package com.university.personalizedLessons.infrastructure.http;
 
 import com.university.personalizedLessons.application.usecases.account.CreateAccount;
+import com.university.personalizedLessons.application.usecases.account.LoginAccount;
+import com.university.personalizedLessons.infrastructure.dto.LoginDTO;
 import com.university.personalizedLessons.infrastructure.dto.RegisterAccountDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,9 @@ public class AccountController {
 
     @Autowired
     CreateAccount createAccount;
+
+    @Autowired
+    LoginAccount loginAccount;
 
     @PostMapping("register")
     public ResponseEntity registerAccount (@RequestBody RegisterAccountDTO data) {
@@ -29,5 +34,18 @@ public class AccountController {
 
         return ResponseEntity.ok(output);
 
+    }
+
+    @PostMapping("login")
+    public ResponseEntity loginAccount (@RequestBody LoginDTO data) {
+
+        LoginAccount.Output output = this.loginAccount.execute(
+                new LoginAccount.Input(
+                        data.username(),
+                        data.password()
+                )
+        );
+
+        return ResponseEntity.ok(output);
     }
 }
