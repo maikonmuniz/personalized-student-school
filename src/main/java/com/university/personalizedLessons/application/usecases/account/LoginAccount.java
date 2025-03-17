b1ccfbe4a8a8d0cbcac3d4590e16e447c0656f26
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-17 02:26:38 -0300

    feat: create use case to login account

--- src/main/java/com/university/personalizedLessons/application/usecases/account/LoginAccount.java
+++ src/main/java/com/university/personalizedLessons/application/usecases/account/LoginAccount.java
@@ -1,4 +1,36 @@
 package com.university.personalizedLessons.application.usecases.account;
 
+import com.university.personalizedLessons.domain.entities.account.Account;
+import com.university.personalizedLessons.infrastructure.repository.AccountRepo;
+import com.university.personalizedLessons.infrastructure.security.TokenAdapter;
+
 public class LoginAccount {
+
+    private final TokenAdapter tokenAdapter;
+    private final AccountRepo accountRepo;
+
+    public LoginAccount (
+            TokenAdapter tokenAdapter,
+            AccountRepo accountRepo
+    ){
+        this.tokenAdapter = tokenAdapter;
+        this.accountRepo = accountRepo;
+    }
+
+    public Output execute (Input input) {
+        Account account = this.accountRepo.findAccount(input.username);
+        String token = this.tokenAdapter.generate(account);
+        return new Output(input.username, token);
+    }
+
+    public static record Input (
+            String username,
+            String password
+    ) { }
+
+    public static record Output (
+            String username,
+            String token
+    ) { }
+
 }
