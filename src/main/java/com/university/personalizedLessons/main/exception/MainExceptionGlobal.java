c0ff26440d55b1a85043ec68f42911c38144d1b3
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-18 20:44:47 -0300

    feat: create instante for framework

--- src/main/java/com/university/personalizedLessons/main/exception/MainExceptionGlobal.java
+++ src/main/java/com/university/personalizedLessons/main/exception/MainExceptionGlobal.java
@@ -1,4 +1,22 @@
 package com.university.personalizedLessons.main.exception;
 
+import com.university.personalizedLessons.infrastructure.exception.ExceptionAdapter;
+import com.university.personalizedLessons.infrastructure.exception.implementation.OperationStatusCode;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainExceptionGlobal {
+
+    @Bean
+    public OperationStatusCode CreateOperationStatusCode () {
+        return new OperationStatusCode ();
+    }
+
+    @Bean
+    public ExceptionAdapter createExceptionAdapter (
+            OperationStatusCode operationStatusCode
+    ) {
+        return new ExceptionAdapter(operationStatusCode);
+    }
 }
