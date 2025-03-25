a3ce3cf6eccb8d9ead65e5b6271efdc95fdb4658
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-25 17:04:31 -0300

    feat: find all coursers

--- src/main/java/com/university/personalizedLessons/application/usecases/course/GetAllCourse.java
+++ src/main/java/com/university/personalizedLessons/application/usecases/course/GetAllCourse.java
@@ -1,4 +1,26 @@
 package com.university.personalizedLessons.application.usecases.course;
 
+import com.university.personalizedLessons.domain.entities.course.Course;
+import com.university.personalizedLessons.infrastructure.repository.CourseRepo;
+
+import java.util.List;
+
 public class GetAllCourse {
+
+    private CourseRepo courseRepo;
+
+    public GetAllCourse (CourseRepo courseRepo) {
+        this.courseRepo = courseRepo;
+
+    }
+
+    public Output execute (Input input) {
+        List<Course> courses = this.courseRepo.findCourseAll(input.start(), input.siza());
+        return new Output(courses);
+    }
+
+    public static record Input (int start, int siza) {}
+
+    public static record Output (List<Course> courseAll) {}
+
 }
