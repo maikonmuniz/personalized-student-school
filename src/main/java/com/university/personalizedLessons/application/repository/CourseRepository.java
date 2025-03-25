d3a947289277ca225a91215822d3f8813fc14829
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-25 17:05:43 -0300

    feat: create create operation course repository inversion depedency JPA

--- src/main/java/com/university/personalizedLessons/application/repository/CourseRepository.java
+++ src/main/java/com/university/personalizedLessons/application/repository/CourseRepository.java
@@ -2,7 +2,10 @@ package com.university.personalizedLessons.application.repository;
 
 import com.university.personalizedLessons.domain.entities.course.Course;
 
+import java.util.List;
+
 public interface CourseRepository {
     Course register (Course course);
     Course findCourse(int id);
+    List<Course> findCourseAll(int start, int size);
 }
