8c583f27a1681d756de520e50f47f78e902b207b
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-25 17:46:22 -0300

    feat: controller course

--- src/main/java/com/university/personalizedLessons/infrastructure/http/CourseController.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/http/CourseController.java
@@ -1,4 +1,20 @@
 package com.university.personalizedLessons.infrastructure.http;
 
+import com.university.personalizedLessons.application.usecases.course.GetAllCourse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("course")
 public class CourseController {
+
+    @Autowired
+    private GetAllCourse getAllCourse;
+
+    @GetMapping("all")
+    public ResponseEntity<GetAllCourse.Output> getAll(@RequestParam int limit, @RequestParam int offset) {
+        GetAllCourse.Output output = this.getAllCourse.execute(new GetAllCourse.Input(limit, offset));
+        return ResponseEntity.ok(output);
+    }
 }
