a42f825786a629c4c16ff80e06b9cd171ecb5041
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-26 18:36:05 -0300

    feat: create implementation for register course

--- src/main/java/com/university/personalizedLessons/application/usecases/course/CreateCourse.java
+++ src/main/java/com/university/personalizedLessons/application/usecases/course/CreateCourse.java
@@ -1,18 +1,25 @@
 package com.university.personalizedLessons.application.usecases.course;
 
-import com.university.personalizedLessons.domain.entities.account.Account;
 import com.university.personalizedLessons.domain.entities.course.Course;
+import com.university.personalizedLessons.infrastructure.exception.ExceptionAdapter;
 import com.university.personalizedLessons.infrastructure.repository.CourseRepo;
 
 public class CreateCourse {
 
     private CourseRepo courseRepo;
+    private ExceptionAdapter exceptionAdapter;
 
-    public CreateCourse(CourseRepo courseRepo) {
+    public CreateCourse(
+            CourseRepo courseRepo,
+            ExceptionAdapter exceptionAdapter
+    ) {
         this.courseRepo = courseRepo;
+        this.exceptionAdapter = exceptionAdapter;
     }
 
     public Output execute (Input input) {
+
+        if (input == null) throw this.exceptionAdapter.badRequest("Nenhum campo enviado!");
 
         Course course = new Course(
                 input.name,
@@ -22,7 +29,11 @@ public class CreateCourse {
 
         Course accountNew = this.courseRepo.register(course);
 
-        return new Output(input.name(), input.description, input.type_course_id);
+        return new Output(
+                accountNew.getName(),
+                accountNew.getDescription(),
+                accountNew.getTypeCourseId()
+        );
     }
 
     static record Input (
