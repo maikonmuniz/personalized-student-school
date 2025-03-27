7d886b258fcc42fbb73bf7bed37206b6d7d76088
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-27 17:50:50 -0300

    feat: create route for register course

--- src/main/java/com/university/personalizedLessons/infrastructure/http/CourseController.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/http/CourseController.java
@@ -1,6 +1,8 @@
 package com.university.personalizedLessons.infrastructure.http;
 
+import com.university.personalizedLessons.application.usecases.course.CreateCourse;
 import com.university.personalizedLessons.application.usecases.course.GetAllCourse;
+import com.university.personalizedLessons.dto.CreateCourseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +14,22 @@ public class CourseController {
     @Autowired
     private GetAllCourse getAllCourse;
 
+    @Autowired
+    private CreateCourse createCourse;
+
+    @PostMapping("register")
+    public ResponseEntity<CreateCourse.Output> createCourse (@RequestBody CreateCourseDTO createCourseDTO) {
+        CreateCourse.Output response = this.createCourse.execute(new CreateCourse.Input(
+                createCourseDTO.name(),
+                createCourseDTO.description(),
+                createCourseDTO.type_course_id()
+        ));
+        return ResponseEntity.ok(response);
+    }
+
     @GetMapping("all")
     public ResponseEntity<GetAllCourse.Output> getAll(@RequestParam int limit, @RequestParam int offset) {
-        GetAllCourse.Output output = this.getAllCourse.execute(new GetAllCourse.Input(limit, offset));
-        return ResponseEntity.ok(output);
+        GetAllCourse.Output response = this.getAllCourse.execute(new GetAllCourse.Input(limit, offset));
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/test/java/com/university/personalizedLessons/application/usecases/course/CreateCourseTest.java
@@ -0,0 +1,7 @@
+package com.university.personalizedLessons.application.usecases.course;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateCourseTest {
+
+}
