62ca8e45831e54b099dc942cb3af1103f79a538a
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-27 19:01:18 -0300

    test: create test for get all course

--- src/test/java/com/university/personalizedLessons/application/usecases/course/CreateCourseTest.java
+++ src/test/java/com/university/personalizedLessons/application/usecases/course/CreateCourseTest.java
@@ -1,7 +1,97 @@
 package com.university.personalizedLessons.application.usecases.course;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.university.personalizedLessons.application.usecases.account.LoginAccount;
+import com.university.personalizedLessons.domain.entities.course.Course;
+import com.university.personalizedLessons.infrastructure.exception.ExceptionAdapter;
+import com.university.personalizedLessons.infrastructure.repository.CourseRepo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class CreateCourseTest {
 
+    @InjectMocks
+    private CreateCourse createCourse;
+
+    @Mock
+    private ExceptionAdapter exceptionAdapter;
+
+    @Mock
+    private CourseRepo courseRepo;
+
+    @Test
+    @DisplayName("Should test if name is null!")
+    public void validationNameCourse() {
+
+        String name = "Sistemas da Informação";
+        String description = "Curso voltado pata àrea de técnologia da informação, com foco em desenvolvimento de software";
+        int type_course = 1;
+
+        when(this.exceptionAdapter.badRequest("Field name is Empty!"))
+                .thenThrow(new RuntimeException("Field name is Empty!"));
+
+        assertThrows(RuntimeException.class, () -> this.createCourse.execute(new CreateCourse.Input(
+                null,
+                description,
+                type_course
+        )));
+
+        verify(exceptionAdapter, times(1)).badRequest("Field name is Empty!");
+
+    }
+
+    @Test
+    @DisplayName("Should test if description is null!")
+    public void validationDescriptionCourse() {
+
+        String name = "Sistemas da Informação";
+        String description = null;
+        int type_course = 1;
+
+        when(this.exceptionAdapter.badRequest("Field description is Empty!"))
+                .thenThrow(new RuntimeException("Field description is Empty!"));
+
+        assertThrows(RuntimeException.class, () -> this.createCourse.execute(new CreateCourse.Input(
+                name,
+                description,
+                type_course
+        )));
+
+        verify(exceptionAdapter, times(1)).badRequest("Field description is Empty!");
+
+    }
+
+    @Test
+    @DisplayName("Should test if register course")
+    public void validationIfCourseRegister() {
+
+        String name = "Sistemas da Informação";
+        String description = "Curso voltado pata àrea de técnologia da informação, com foco em desenvolvimento de software";
+        int type_course = 1;
+
+        String message = "No register course!";
+
+        when(this.exceptionAdapter.badRequest(message))
+                .thenThrow(new RuntimeException(message));
+
+        CreateCourse.Input input = new CreateCourse.Input(
+                name,
+                description,
+                type_course
+        );
+
+        when(this.courseRepo.register(any(Course.class))).thenReturn(null);
+
+        assertThrows(RuntimeException.class, () -> this.createCourse.execute(input));
+
+        verify(exceptionAdapter, times(1)).badRequest(message);
+
+    }
 }
