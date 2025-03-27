4065bf38de9ca29d4d13d14d81a9896cf5e03f9e
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-27 17:51:32 -0300

    feat: create DTO for resquest in controller the course

--- src/main/java/com/university/personalizedLessons/dto/CreateCourseDTO.java
+++ src/main/java/com/university/personalizedLessons/dto/CreateCourseDTO.java
@@ -1,4 +1,7 @@
 package com.university.personalizedLessons.dto;
 
-public record CreateCourseDTO() {
-}
+public record CreateCourseDTO(
+        String name,
+        String description,
+        int type_course_id
+) { }
