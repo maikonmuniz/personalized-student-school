2c4ef2c1877fb11743970ebf777f201b0ee09a06
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-31 01:20:09 -0300

    feat: create model class course account

---
+++ src/main/java/com/university/personalizedLessons/infrastructure/models/ClassCourseAccount.java
@@ -0,0 +1,60 @@
+package com.university.personalizedLessons.infrastructure.models;
+
+import com.university.personalizedLessons.domain.entities.classCourse.ClassCourse;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "class_course_account")
+@Entity(name = "class_course_account")
+public class ClassCourseAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "presence")
+    private boolean presence;
+
+    @ManyToOne
+    @JoinColumn(name = "id_account")
+    private AccountModel accountModel;
+
+    @ManyToOne
+    @JoinColumn(name = "id_class_course")
+    private ClassCourse classCourse;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isPresence() {
+        return presence;
+    }
+
+    public void setPresence(boolean presence) {
+        this.presence = presence;
+    }
+
+    public AccountModel getAccountModel() {
+        return accountModel;
+    }
+
+    public void setAccountModel(AccountModel accountModel) {
+        this.accountModel = accountModel;
+    }
+
+    public ClassCourse getClassCourse() {
+        return classCourse;
+    }
+
+    public void setClassCourse(ClassCourse classCourse) {
+        this.classCourse = classCourse;
+    }
+}
