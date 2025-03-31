866b45b4ec85a1a7127d416bb85974587db2ba9b
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-31 02:17:51 -0300

    feat: create model test

--- src/main/java/com/university/personalizedLessons/infrastructure/models/Test.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/models/Test.java
@@ -1,4 +1,48 @@
 package com.university.personalizedLessons.infrastructure.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "test")
+@Table(name = "test")
 public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_type_note")
+    private TypeNote typeNote;
+
+    @ManyToOne
+    @JoinColumn(name = "id_class_course_account")
+    private ClassCourseAccount classCourseAccount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public TypeNote getTypeNote() {
+        return typeNote;
+    }
+
+    public void setTypeNote(TypeNote typeNote) {
+        this.typeNote = typeNote;
+    }
+
+    public ClassCourseAccount getClassCourseAccount() {
+        return classCourseAccount;
+    }
+
+    public void setClassCourseAccount(ClassCourseAccount classCourseAccount) {
+        this.classCourseAccount = classCourseAccount;
+    }
 }
