85a15d2cf9c675344ceed7c172b8e4f726063f1f
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-03-31 08:16:32 -0300

    feat: rename Course to CourseAggregate and create Entity Discipline

--- src/main/java/com/university/personalizedLessons/domain/entities/course/CourseAgraggate.java
+++ src/main/java/com/university/personalizedLessons/domain/entities/course/CourseAgraggate.java
@@ -1,23 +1,17 @@
 package com.university.personalizedLessons.domain.entities.course;
 
-public class Course {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseAgraggate {
 
     private int id;
     private final String name;
     private final int typeCourseId;
     private final String description;
+    private List<Discipline> listDiscipline;
 
-    public Course(
-            String name,
-            String description,
-            int typeCourseId
-    ){
-        this.name = name;
-        this.description = description;
-        this.typeCourseId = typeCourseId;
-    }
-
-    public Course(
+    public CourseAgraggate(
             int id,
             String name,
             String description,
@@ -43,5 +37,13 @@ public class Course {
 
     public int getId() {
         return id;
+    }
+
+    public List<Discipline> getListDiscipline() {
+        return listDiscipline;
+    }
+
+    public void setListDiscipline(List<Discipline> listDiscipline) {
+        this.listDiscipline = listDiscipline;
     }
 }
