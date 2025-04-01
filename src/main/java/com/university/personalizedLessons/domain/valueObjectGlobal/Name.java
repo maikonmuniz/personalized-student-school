c440238fb2ec13ed3f55a2af6ab1305782b92598
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-01 15:34:11 -0300

    feat: create value onject name global

---
+++ src/main/java/com/university/personalizedLessons/domain/valueObjectGlobal/Name.java
@@ -0,0 +1,28 @@
+package com.university.personalizedLessons.domain.valueObjectGlobal;
+
+public class Name {
+
+    /**
+     *
+     * This value is global only for validation to check if the value is empty.
+     */
+    private String value;
+    private boolean isValidation;
+
+    public Name (String value) {
+        this.value = value;
+        this.isValidation = this.isValidationValue();
+    }
+
+    private boolean isValidationValue () {
+        return this.value.isEmpty() || this.value == "";
+    }
+
+    public String getName () {
+        return value;
+    }
+
+    public boolean getIsValidation() {
+        return this.isValidation;
+    }
+}
