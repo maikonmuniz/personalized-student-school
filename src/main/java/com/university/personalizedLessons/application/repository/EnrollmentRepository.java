d4a88c0dd3b07e0066238a4ffe9573fa1e3db468
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-10 04:40:36 -0300

    feat: add operation datatabase

--- src/main/java/com/university/personalizedLessons/application/repository/EnrollmentRepository.java
+++ src/main/java/com/university/personalizedLessons/application/repository/EnrollmentRepository.java
@@ -1,9 +1,8 @@
 package com.university.personalizedLessons.application.repository;
 
-import com.university.personalizedLessons.domain.domainServices.Enrollment;
-import com.university.personalizedLessons.domain.entities.account.Account;
-import com.university.personalizedLessons.domain.entities.course.CourseAggregate;
+
+import com.university.personalizedLessons.domain.entities.enrollment.Enrollment;
 
 public interface EnrollmentRepository {
-    Enrollment save (Account account, CourseAggregate courseAggregate);
+    Enrollment save (Enrollment enrollment);
 }
