5ebc6e74c0dbe6d3be2372a373d1bca1f4fcc835
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-11 23:52:29 -0300

    test: implememtation validate fields

--- src/test/java/com/university/personalizedLessons/application/usecases/account/AccountAcceptDisciplineTest.java
+++ src/test/java/com/university/personalizedLessons/application/usecases/account/AccountAcceptDisciplineTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class AccountAcceptDisciplineTest {
@@ -25,7 +26,6 @@ class AccountAcceptDisciplineTest {
         String accountID = "2a3e0c4b-5d8b-46a0-901d-b712df3d6a2a";
         String disciplineID = "c90f0e7b-66db-4f0c-b7b4-830fa71c5c3e";
 
-
         AccountAcceptDiscipline.Input input = new AccountAcceptDiscipline.Input(
                 courseID,
                 accountID,
@@ -37,5 +37,42 @@ class AccountAcceptDisciplineTest {
         assertEquals(courseID, output.courseID());
         assertEquals(accountID, output.accountID());
         assertEquals("Selection disciplines finish!", output.message());
+    }
+
+    @Test
+    void shouldThrowBadRequestWhenAccountIDIsEmpty() {
+
+        String courseID = "f4a5a3e0-1c1b-4c59-8c69-9c6c4de1b1f0";
+        String accountID = "2a3e0c4b-5d8b-46a0-901d-b712df3d6a2a";
+        String disciplineID = "c90f0e7b-66db-4f0c-b7b4-830fa71c5c3e";
+
+        AccountAcceptDiscipline.Input input = new AccountAcceptDiscipline.Input(
+                courseID,
+                "",
+                disciplineID
+        );
+
+        RuntimeException expectedException = new RuntimeException("Field account id is empty!");
+        when(exceptionAdapter.badRequest("Field account id is empty!")).thenThrow(expectedException);
+
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> this.accountAcceptDiscipline.execute(input));
+        assertEquals("Field account id is empty!", thrown.getMessage());
+        verify(exceptionAdapter).badRequest("Field account id is empty!");
+    }
+
+    @Test
+    void shouldThrowBadRequestWhenCourseIDIsEmpty() {
+
+        String courseID = "f4a5a3e0-1c1b-4c59-8c69-9c6c4de1b1f0";
+        String accountID = "2a3e0c4b-5d8b-46a0-901d-b712df3d6a2a";
+        String disciplineID = "c90f0e7b-66db-4f0c-b7b4-830fa71c5c3e";
+
+        AccountAcceptDiscipline.Input input = new AccountAcceptDiscipline.Input("", accountID, disciplineID);
+        RuntimeException expectedException = new RuntimeException("Field course id is empty!");
+        when(exceptionAdapter.badRequest("Field course id is empty!")).thenThrow(expectedException);
+
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> this.accountAcceptDiscipline.execute(input));
+        assertEquals("Field course id is empty!", thrown.getMessage());
+        verify(exceptionAdapter).badRequest("Field course id is empty!");
     }
 }
