2a3d0b2cd119f5fd4fa04306ef192c657c8d5aa9
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-11 23:54:18 -0300

    feat: account discipline operation jpa

--- src/main/java/com/university/personalizedLessons/infrastructure/operationORM/AccountDisciplineJpa.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/operationORM/AccountDisciplineJpa.java
@@ -3,5 +3,4 @@ package com.university.personalizedLessons.infrastructure.operationORM;
 import com.university.personalizedLessons.infrastructure.models.AccountDisciplineModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountDiscipline extends JpaRepository<AccountDisciplineModel, Long> {
-}
+public interface AccountDisciplineJpa extends JpaRepository<AccountDisciplineModel, Long> { }
