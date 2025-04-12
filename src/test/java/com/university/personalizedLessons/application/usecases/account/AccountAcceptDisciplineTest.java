2f70a7ac02942badfc4e824dae057997e1025731
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-11 23:59:17 -0300

    test: implementation code for test all field receive

--- src/test/java/com/university/personalizedLessons/application/usecases/account/AccountAcceptDisciplineTest.java
+++ src/test/java/com/university/personalizedLessons/application/usecases/account/AccountAcceptDisciplineTest.java
@@ -75,4 +75,20 @@ class AccountAcceptDisciplineTest {
         assertEquals("Field course id is empty!", thrown.getMessage());
         verify(exceptionAdapter).badRequest("Field course id is empty!");
     }
+
+    @Test
+    void shouldThrowBadRequestWhenDisciplineIDIsEmpty() {
+
+        String courseID = "f4a5a3e0-1c1b-4c59-8c69-9c6c4de1b1f0";
+        String accountID = "2a3e0c4b-5d8b-46a0-901d-b712df3d6a2a";
+        String disciplineID = "c90f0e7b-66db-4f0c-b7b4-830fa71c5c3e";
+
+        AccountAcceptDiscipline.Input input = new AccountAcceptDiscipline.Input(courseID, accountID, "");
+        RuntimeException expectedException = new RuntimeException("Field discipline id is empty!");
+        when(exceptionAdapter.badRequest("Field discipline id is empty!")).thenThrow(expectedException);
+
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> this.accountAcceptDiscipline.execute(input));
+        assertEquals("Field discipline id is empty!", thrown.getMessage());
+        verify(exceptionAdapter).badRequest("Field discipline id is empty!");
+    }
 }
