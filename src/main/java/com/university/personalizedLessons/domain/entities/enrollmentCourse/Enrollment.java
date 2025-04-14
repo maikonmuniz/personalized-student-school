feb82c1df557a84cb4eb0a2c44b04bf9e3170d57
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-14 18:27:43 -0300

    feat: create entity register account in course

--- src/main/java/com/university/personalizedLessons/domain/entities/enrollmentCourse/Enrollment.java
+++ src/main/java/com/university/personalizedLessons/domain/entities/enrollmentCourse/Enrollment.java
@@ -1,4 +1,4 @@
-package com.university.personalizedLessons.domain.entities.enrollment;
+package com.university.personalizedLessons.domain.entities.enrollmentCourse;
 
 import java.time.LocalDate;
 import java.util.UUID;
