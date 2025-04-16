4221a7151717eba9d77a91e14d4c13f9d480d3e3
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-15 21:25:28 -0300

    feat: class course jpa, create consult by id

--- src/main/java/com/university/personalizedLessons/infrastructure/operationORM/ClassCourseJPA.java
+++ src/main/java/com/university/personalizedLessons/infrastructure/operationORM/ClassCourseJPA.java
@@ -2,5 +2,10 @@ package com.university.personalizedLessons.infrastructure.operationORM;
 
 import com.university.personalizedLessons.infrastructure.models.ClassCourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ClassCourseJPA extends JpaRepository <ClassCourseModel, Long> { }
+public interface ClassCourseJPA extends JpaRepository <ClassCourseModel, Long> {
+    @Query(value = "SELECT * FROM class_course c where c.class_id = :id;", nativeQuery = true)
+    ClassCourseModel consultClassId(@Param("id") String id);
+}
