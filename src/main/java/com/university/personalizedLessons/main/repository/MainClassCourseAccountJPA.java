66eb3fac1482f90d12dab4e3b516d25810df1743
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-15 21:19:34 -0300

    feat: create bean repository

--- src/main/java/com/university/personalizedLessons/main/repository/MainClassCourseAccountJPA.java
+++ src/main/java/com/university/personalizedLessons/main/repository/MainClassCourseAccountJPA.java
@@ -1,4 +1,25 @@
 package com.university.personalizedLessons.main.repository;
 
+import com.university.personalizedLessons.infrastructure.operationORM.AccountJPA;
+import com.university.personalizedLessons.infrastructure.operationORM.ClassCourseAccountJPA;
+import com.university.personalizedLessons.infrastructure.operationORM.ClassCourseJPA;
+import com.university.personalizedLessons.infrastructure.repository.ClassCourseAccountRepo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainClassCourseAccountJPA {
+
+    @Bean
+    ClassCourseAccountRepo createClassCourseAccountRepo (
+            ClassCourseAccountJPA classAccountJPA,
+            ClassCourseJPA classCourseJPA,
+            AccountJPA accountJPA
+    ) {
+        return new ClassCourseAccountRepo(
+                classAccountJPA,
+                classCourseJPA,
+                accountJPA
+        );
+    }
 }
