a4711797b2f1f9bcbbe7e6c855eb4fd23ea4290c
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-15 21:19:03 -0300

    feat: create value object presence

--- src/main/java/com/university/personalizedLessons/domain/entities/classCourse/vo/Presence.java
+++ src/main/java/com/university/personalizedLessons/domain/entities/classCourse/vo/Presence.java
@@ -1,4 +1,14 @@
 package com.university.personalizedLessons.domain.entities.classCourse.vo;
 
 public class Presence {
+
+    private boolean value;
+
+    public Presence (boolean value) {
+        this.value = value;
+    }
+
+    public boolean getValue () {
+        return value;
+    }
 }
