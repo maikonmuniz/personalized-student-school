de0721b78b651d42c75aa53d4a4951ee26036cf5
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-16 00:37:36 -0300

    feat: implementation validation if account is student

--- src/main/java/com/university/personalizedLessons/application/usecases/classCourse/AcceptClass.java
+++ src/main/java/com/university/personalizedLessons/application/usecases/classCourse/AcceptClass.java
@@ -1,18 +1,34 @@
 package com.university.personalizedLessons.application.usecases.classCourse;
 
+import com.university.personalizedLessons.application.repository.AccountRepository;
+import com.university.personalizedLessons.domain.entities.account.Account;
 import com.university.personalizedLessons.infrastructure.exception.ExceptionAdapter;
 
 public class AcceptClass {
 
     private ExceptionAdapter exception;
+    private AccountRepository accountRepo;
 
-    public AcceptClass (ExceptionAdapter exception) {
+    public AcceptClass (
+            ExceptionAdapter exception,
+            AccountRepository accountRepo
+    ) {
         this.exception = exception;
+        this.accountRepo = accountRepo;
     }
 
     public Output execute (Input input) {
         if (input.studentID.isEmpty()) throw this.exception.badRequest("Field is empty!");
         if (input.classID.isEmpty()) throw this.exception.badRequest("Field is empty!");
+
+        Account account = this.accountRepo.findOneId(input.studentID);
+
+        if (account == null) throw this.exception.badRequest("No exist account");
+
+        boolean validationIfAccountStudent = account.validationStudent();
+
+        if (!validationIfAccountStudent) throw this.exception.badRequest("No permission account for accept class!");
+
         return new Output(input.studentID, input.classID);
     }
 
