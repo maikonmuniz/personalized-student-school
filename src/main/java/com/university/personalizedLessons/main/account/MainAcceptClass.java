b5b5715b51b45c6d9f739e29881afe32502e61e1
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-17 01:23:02 -0300

    feat: create config for bean in project

--- src/main/java/com/university/personalizedLessons/main/account/MainAcceptClass.java
+++ src/main/java/com/university/personalizedLessons/main/account/MainAcceptClass.java
@@ -1,22 +1,25 @@
 package com.university.personalizedLessons.main.account;
 
 import com.university.personalizedLessons.application.repository.AccountRepository;
+import com.university.personalizedLessons.application.repository.ClassCourseAccountRepository;
+import com.university.personalizedLessons.application.usecases.classCourse.AcceptClass;
 import com.university.personalizedLessons.infrastructure.exception.ExceptionAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AcceptClass {
+public class MainAcceptClass {
 
     @Bean
     AcceptClass createAcceptClass (
             ExceptionAdapter exception,
-            AccountRepository accountRepo
+            AccountRepository accountRepo,
+            ClassCourseAccountRepository classCourseAccountRepo
     ) {
-        return new AcceptClass(
+        return new AcceptClass (
                 exception,
-                accountRepo
+                accountRepo,
+                classCourseAccountRepo
         );
     }
-
 }
