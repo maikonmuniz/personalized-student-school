a950e0cc97bf756831bb1f1add2227305d3e86c9
maikon.muniz <maikon.muniz@yahoo.com.br>
2026-04-21 14:28:40 -0300

    feat: install docker compose

--- src/main/java/com/university/personalizedLessons/PersonalizedLessonsApplication.java
+++ src/main/java/com/university/personalizedLessons/PersonalizedLessonsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonalizedLessonsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalizedLessonsApplication.class, args);
 	}
